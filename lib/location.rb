255b8c294bc611ad2fda76b3d5955c1bf2079666
richo <richo@psych0tik.net>
2013-01-27 04:14:55 +1100

    Create Location class

---
+++ lib/location.rb
@@ -0,0 +1,16 @@
+class Location
+
+  def initialize(loc)
+    @loc = loc
+  end
+  attr_accessor :loc
+
+  def postcode
+    # TODO
+  end
+
+  def latlong
+    # TODO
+  end
+
+end
--- lib/user_location.rb
+++ lib/user_location.rb
@@ -1,15 +1,2 @@
-class UserLocation
-  def initialize(loc)
-    @loc = loc
-  end
-  attr_accessor :loc
-
-  def postcode
-    # TODO
-  end
-
-  def latlong
-    # TODO
-  end
-
+class UserLocation < Location
 end
