2a8d99a5e2928d03f990c58f9107786250f40068
richo <richo@psych0tik.net>
2013-01-27 01:53:25 +1100

    Add partially broken woolworths engine

--- lib/engines/nowwhere_engine.rb
+++ lib/engines/nowwhere_engine.rb
@@ -1,4 +1,40 @@
 module Engines
   class NowWhere < Base
+    def backend_url
+      "http://apps.nowwhere.com.au/#{nowwhere_app_name}/locator/FindNearest.ashx"
+    end
+
+    def query_params
+      {
+        "x" => lat,
+        "y" => lon,
+        "storeType" => ""
+      }
+    end
+
+    def query_headers
+      {
+        "Referer" => "http://apps.nowwhere.com.au/woolworths/locator/default.aspx"
+      }
+    end
+
+    def stores
+      uri = URI(backend_url)
+      uri.query = URI.encode_www_form(query_params)
+
+      req = Net::HTTP::Get.new(uri.request_uri)
+      query_headers.each do |k, v|
+        req[k] = v
+      end
+
+      resp= Net::HTTP.start(uri.host, uri.port) do |http|
+        http.request req
+      end
+
+      if resp.is_a?(Net::HTTPSuccess)
+        data = JSON.parse(resp.body)
+      end
+    end
+
   end
 end
--- lib/engines/woolworths_engine.rb
+++ lib/engines/woolworths_engine.rb
@@ -1,4 +1,15 @@
 module Engines
   class Woolworths < Base
+    def nowwhere_app_name
+      "woolworths".freeze
+    end
+
+    def lat
+      location.latlong[:lat]
+    end
+
+    def lon
+      location.latlong[:lon]
+    end
   end
 end
