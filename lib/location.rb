f4fced68fb1583abd30b6ffcc2d5cdee8cda308e
richo <richo@psych0tik.net>
2013-01-28 03:03:30 +1100

    Refactor location to lookup from postcodes dataset

--- lib/location.rb
+++ lib/location.rb
@@ -1,16 +1,36 @@
 class Location
 
-  def initialize(loc)
-    @loc = loc
+  def self.from_postcode(postcode)
+    new(:postcode => postcode)
   end
-  attr_accessor :loc
+
+  def initialize(opts)
+    @opts = opts
+  end
+  attr_accessor :opts
 
   def postcode
-    # TODO
+    @postcode ||= opts[:postcode].to_i || get_postcode
   end
 
-  def latlong
-    # TODO
+  def lat
+    @lat ||= opts[:lat] || get_lat
+  end
+
+  def lon
+    @lon ||= opts[:lon] || get_lon
+  end
+
+  def get_postcode
+    raise "Not implemented"
+  end
+
+  def get_lat
+    POSTCODES[postcode][4]
+  end
+
+  def get_lon
+    POSTCODES[postcode][5]
   end
 
 end
