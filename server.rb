13b3d0101ac2ab8ca41360a3977e34af48e17ed2
richo <richo@psych0tik.net>
2013-01-28 02:58:13 +1100

    Adjust loader to support directories other than lib/

--- server.rb
+++ server.rb
@@ -4,14 +4,16 @@ require 'net/http'
 require 'sinatra'
 
 def load_all_in(path)
-  Dir["#{File.expand_path("../lib/#{path}", __FILE__)}/*.rb"].each do |file|
+  Dir["#{File.expand_path("../#{path}", __FILE__)}/*.rb"].each do |file|
     require file
   end
 end
 
-load_all_in("")
-load_all_in("engines")
-load_all_in("controllers")
+
+load_all_in("data")
+load_all_in("lib")
+load_all_in("lib/engines")
+load_all_in("lib/controllers")
 
 class IsItOpen < Sinatra::Base
 
