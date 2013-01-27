3ac64eaf1efce7d8c02e6478123eaae8095602d7
richo <richo@psych0tik.net>
2013-01-28 03:06:40 +1100

    Wire Search controller up to views/location model

--- lib/controllers/search_controller.rb
+++ lib/controllers/search_controller.rb
@@ -1,7 +1,21 @@
 class SearchController < BaseController
 
   def search
-    return []
+    return engine.stores(location)
+  end
+
+  def location
+    raise "No location given" unless params[:postcode] # TODO
+    UserLocation.from_postcode(params[:postcode])
+  end
+
+  def engine
+    store = params[:store]
+    unless Engines::ENGINES.include? store
+      raise "Invalid store"
+    end
+
+    @engine ||= Engines::ENGINES[store].new
   end
 
 end
