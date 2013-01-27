b78b95ed806e9374aea98fdaf11d2a719547d4c6
richo <richo@psych0tik.net>
2013-01-28 03:10:54 +1100

    Remove unused properties, add displayname

--- lib/engines/woolworths_engine.rb
+++ lib/engines/woolworths_engine.rb
@@ -1,16 +1,14 @@
 module Engines
   class Woolworths < NowWhere
 
+    class << self
+      def display_name
+        "Woolworths"
+      end
+    end
+
     def nowwhere_app_name
       "woolworths".freeze
-    end
-
-    def lat
-      location.latlong[:lat]
-    end
-
-    def lon
-      location.latlong[:lon]
     end
 
   end
