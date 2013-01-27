5f4c951b5c4685ebae30d50af50c2b57ce50b0fa
richo <richo@psych0tik.net>
2013-01-28 03:00:45 +1100

    Refactor query_params out of nowwhere engine

--- lib/engines/nowwhere_engine.rb
+++ lib/engines/nowwhere_engine.rb
@@ -4,22 +4,14 @@ module Engines
       "http://apps.nowwhere.com.au/#{nowwhere_app_name}/locator/FindNearest.ashx"
     end
 
-    def query_params
-      {
-        "x" => lat,
-        "y" => lon,
-        "storeType" => ""
-      }
-    end
-
     def query_headers
       {
         "Referer" => "http://apps.nowwhere.com.au/woolworths/locator/default.aspx"
       }
     end
 
-    def stores
-      fetch_data.map do |record|
+    def stores(loc)
+      fetch_data(loc).map do |record|
         Store.new(:hours => get_hours(record), :location => record["Street"])
       end
     end
@@ -36,9 +28,13 @@ module Engines
       }
     end
 
-    def fetch_data
+    def fetch_data(loc)
       uri = URI(backend_url)
-      uri.query = URI.encode_www_form(query_params)
+      uri.query = URI.encode_www_form({
+        "x" => loc.lat,
+        "y" => loc.lon,
+        "storeType" => ""
+      })
 
       req = Net::HTTP::Get.new(uri.request_uri)
       query_headers.each do |k, v|
@@ -53,7 +49,9 @@ module Engines
         respbody = resp.body
         respbody.sub!(/\A[^\[]*/, "")
         respbody.sub!(/[^\]]*\z/, "")
-        data = JSON.load(respbody)
+        return JSON.load(respbody)
+      else
+        raise "Backend returned #{resp.inspect}"
       end
     end
 
