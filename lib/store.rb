9c08c04076db0fe9ed2a3bbe9ae49c07baec10c4
richo <richo@psych0tik.net>
2013-01-28 21:13:09 +1100

    Pending spec for overnight hours

--- lib/store.rb
+++ lib/store.rb
@@ -2,4 +2,5 @@ class Store < Struct.new(:hours, :location)
   def initialize(opts)
     super(opts[:hours], opts[:location])
   end
+
 end
--- spec/hours_spec.rb
+++ spec/hours_spec.rb
@@ -10,4 +10,16 @@ describe Hours do
     hours.should include now
 
   end
+
+  it "Should include times the next morning" do
+    pending
+    now = Time.mktime(2013, 1, 22, 3) # 3am on a tuesday
+
+    from = TimeWithoutDate.new(8, 00)
+    to = TimeWithoutDate.new(4, 00)
+    hours = Hours.new(from, to)
+
+    hours.should include now
+
+  end
 end
